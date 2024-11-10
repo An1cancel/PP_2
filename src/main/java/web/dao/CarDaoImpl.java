04f80228c54fa71070967cb69abea8ade769613c
An1cancel <firesfire666@gmail.com>
2024-11-10 21:36:45 +0300

    2.2.2(v2)

--- src/main/java/web/dao/CarDaoImpl.java
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -1,23 +1,24 @@
-package web.controller;
+package web.dao;
 
 import org.springframework.stereotype.Service;
+import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class CarService {
+public class CarDaoImpl implements CarDao{
 
     private List<Car> carList = new ArrayList<Car>();
 
-    public CarService() {
+    public CarDaoImpl() {
         carList.add(new Car("Toyota", "Corolla", 2020));
         carList.add(new Car("Honda", "Civic", 2019));
         carList.add(new Car("Ford", "Focus", 2018));
         carList.add(new Car("Chevrolet", "Cruze", 2017));
         carList.add(new Car("BMW", "3 Series", 2016));
     }
-
+    @Override
     public List<Car> getCarList(Integer count) {
         if (count == null || count >=carList.size()){
             return carList;
---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,22 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import web.dao.CarDao;
+import web.model.Car;
+
+import java.util.List;
+@Repository
+public class CarServiceImpl implements CarService {
+
+    private final CarDao carDao;
+    @Autowired
+    public CarServiceImpl(CarDao carDao) {
+        this.carDao = carDao;
+    }
+
+    @Override
+    public List<Car> getCarList(Integer count){
+        return carDao.getCarList(count);
+    }
+}
