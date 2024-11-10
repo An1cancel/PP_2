548cfee92004638c308983b8e1189c8abcb28d8e
An1cancel <firesfire666@gmail.com>
2024-11-10 21:13:02 +0300

    2.3.1

---
+++ src/main/java/web/controller/MyController.java
@@ -0,0 +1,70 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.service.UserService;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/users")
+public class MyController {
+
+    private final UserService userService;
+
+    @Autowired
+    public MyController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public String allUser(Model model){
+        model.addAttribute("users",userService.getUsersList());
+        return "users/all_users";
+    }
+
+    @GetMapping("/show")
+    public String showUser(@RequestParam("id") int id, Model model){
+        model.addAttribute("user",userService.getUser(id));
+        return "users/show";
+    }
+
+    @GetMapping("/new")
+    public String newUser(@ModelAttribute("user") User user){
+        return "users/new";
+    }
+
+    @PostMapping("/new")
+    public String create(@ModelAttribute("user") @Valid User user, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "users/new";
+        }
+        userService.addUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/edit")
+    public String editUser(@RequestParam("id") int id, Model model){
+        model.addAttribute("user",userService.getUser(id));
+        return "users/edit";
+    }
+
+    @PostMapping("/edit")
+    public String update(@ModelAttribute("user") @Valid User user, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "users/edit";
+        }
+        userService.editUser(user);
+        return "redirect:/users";
+    }
+
+    @PostMapping("/delete")
+    public String delete (@RequestParam("id") int id){
+        userService.deleteUser(id);
+        return "redirect:/users";
+    }
+}
